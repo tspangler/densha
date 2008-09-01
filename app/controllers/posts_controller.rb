7e5d23e4feb421a7b9aba3e017732773fb27942d
symsonic <symsonic@c96d977a-a3c3-4c09-b5ac-e7506fd20091>
2008-09-01 15:45:01 +0000

    Created skeleton defs in controller and added minor validation to posts model.
    git-svn-id: https://symsonic.svn.beanstalkapp.com/chichan@2 c96d977a-a3c3-4c09-b5ac-e7506fd20091

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,9 @@
 class PostsController < ApplicationController
+  def create
+    @post = Post.new(:params[:post])
+  end
+  
+  def delete
+    # Psuedocode: if session.admin? delete end
+  end
 end
