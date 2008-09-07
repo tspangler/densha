e697499003d3f59a12b1e6488051e9f0151d09ad
symsonic <symsonic@c96d977a-a3c3-4c09-b5ac-e7506fd20091>
2008-09-07 02:25:27 +0000

    Fixed board posting! Thanks to a hidden field the shortcode is now correctly passed to the posts controller.
    Changed stylesheet to one-line declarations.
    Added subject field to posting form.
    
    git-svn-id: https://symsonic.svn.beanstalkapp.com/chichan@5 c96d977a-a3c3-4c09-b5ac-e7506fd20091

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,9 @@
 class PostsController < ApplicationController
   def create
     @post = Post.new(params[:post])
-    # TODO: Make this actually save. Something's getting confusing about shortcodes.
-    # Should we figure out the parent board by shortcode or id?
-    # I say shortcode, but for some reason I can't get it to pass through.
     
     if @post.save
-      flash[:notice] = "Post added to board #{@parent_board}"
+      flash[:notice] = "Post added to board #{params[:shortcode]}"
       redirect_to :back
     else
       render :action => 'create'
