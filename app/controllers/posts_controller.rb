fba6b6de449f3fad0c72b35d627f6c6420fc8350
symsonic <symsonic@c96d977a-a3c3-4c09-b5ac-e7506fd20091>
2008-09-07 06:46:16 +0000

    Removed hidden form field for determining proper board from last revision Changed post controller to correctly add shortcode before saving posts
    git-svn-id: https://symsonic.svn.beanstalkapp.com/chichan@7 c96d977a-a3c3-4c09-b5ac-e7506fd20091

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,9 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(params[:post])
     
+    # Move the shortcode parameter into the @post hash so it gets saved
+    @post.parent_board = params[:shortcode];
+    
     if @post.save
       flash[:notice] = "Post added to board #{params[:shortcode]}"
       redirect_to :back
